63b9af466d3674b0a9489d9b3c2e388c19f7690a
imoladezsiMacadamian <ikatalin-dezsi@macadamian.com>
2018-07-10 11:23:37 +0300

    new folder

---
+++ dezsi/Hashing/Hashing/Generate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+
+
+    class Generate
+    {
+        static List<string> values = new List<string>();
+
+        public static List<string> GeneratePermutations(int passlen) {
+           DivePermutations("", 0, passlen);
+
+            return values;
+
+        }
+
+       
+       
+        private static void DivePermutations(string prefix, int level, int maxlength)
+        {
+            level += 1;
+ 
+            char[] alphas = "abcdefghijklmnopqrstuvwxyz".ToArray();
+            foreach (char c in alphas){
+                // Console.WriteLine(prefix + c);
+                if (level < maxlength)
+                {
+                    DivePermutations(prefix + c, level, maxlength);
+                }
+                else
+                {
+                   // Console.WriteLine(prefix + c);
+
+
+
+
+
+
+
+                    values.Add(prefix + c);
+                }
+            }
+        }
+
+
+    }
+
+
+}
---
+++ dezsi/HashingEntity/HashingEntity/Generate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingEntity
+{
+    class Generate
+    {
+        static List<string> values = new List<string>();
+
+        public static List<string> GenerateArrangements(int passlen)
+        {
+
+            DiveArrangements("", 0, passlen);
+
+            return values;
+
+        }
+
+
+
+        private static void DiveArrangements(string prefix, int level, int maxlength)
+        {
+            level += 1;
+
+            char[] alphas = "abcdefghijklmnopqrstuvwxyz".ToArray();
+            foreach (char c in alphas)
+            {
+                // Console.WriteLine(prefix + c);
+                if (level < maxlength)
+                {
+                    DiveArrangements(prefix + c, level, maxlength);
+                }
+                else
+                {
+                    // Console.WriteLine(prefix + c);
+
+
+                    values.Add(prefix + c);
+                }
+            }
+        }
+
+
+    }
+
+}
