2d68e777c9f33513098ad93da2524fd54fedf8fe
LsRobert1 <41046787+LsRobert1@users.noreply.github.com>
2018-07-13 15:01:36 +0300

    Client/Server app

---
+++ LsRobert/MessApp(ClientServer)/Client/IChat.cs
@@ -0,0 +1,19 @@
+﻿using Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Client.ChatService;
+using static Client.Events.Handles;
+
+namespace Client
+{
+    interface IChat
+    {
+         void sendMessage(String s);
+        List<String> getListOfUsers();
+
+        event EventHandler<SendMessageArgs> SendedMessage;
+    }
+}
