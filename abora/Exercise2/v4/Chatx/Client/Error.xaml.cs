5a24b6cc2bd10ae8e25d61ce9136203f454684c9
abora13 <abora@macadamian.com>
2018-07-17 13:19:29 +0300

    Refactor code and add events

---
+++ abora/Exercise2/v4/Chatx/Client/Error.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for Error.xaml
+    /// </summary>
+    public partial class Error : Window
+    {
+        public Error()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+    }
+}
