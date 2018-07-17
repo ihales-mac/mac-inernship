89e391d0c919342bd58c3e84e1eeb8627f4d7cf8
imoladezsiMacadamian <41046028+imoladezsiMacadamian@users.noreply.github.com>
2018-07-17 15:52:38 +0300

    Imola dezsi (#9)
    * gitignore
    
    * Requirement2
    
    * asymmetric encription

---
+++ dezsi/Requirement2/ServerApp/ServerApp/ICommunication.cs
@@ -0,0 +1,22 @@
+﻿using CommonApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    interface ICommunication<T>
+    {
+       // void SendMessage(T handler, String msg, Header type);
+        string ResolveLogin(String username, String Password);
+        void StartListening();
+        //Generic send method to be used
+        void UnpackAndSend(T handler, string message, Header type);
+       
+       
+      
+    }
+}
---
+++ dezsi/Requirement2/ServerApp/ServerApp/SocketNp/ThreadedSocket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+
+    public class HandleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        public void startClient(Socket inClientSocket, string clineNo)
+        {
+
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[1024];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while ((true))
+            {
+                //try
+               // {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, (int)bytesFrom.Length);
+
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
+
+                    rCount = Convert.ToString(requestCount);
+                    serverResponse = "Server to client(" + clNo + ") " + rCount;
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    Console.WriteLine(sendBytes);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                //}
+              //  catch (Exception ex)
+               // {
+               //     Console.WriteLine(" >> " + ex.ToString());
+               // }
+            }
+        }
+
+    }
+
+}
