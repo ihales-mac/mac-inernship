3c2a3a4825ba7c9314e528d9eceb3beb1e38eb0b
imoladezsiMacadamian <41046028+imoladezsiMacadamian@users.noreply.github.com>
2018-07-18 17:59:08 +0300

    Imola dezsi (#12)
    * gitignore
    
    * Requirement2
    
    * asymmetric encription
    
    * refactoring
    
    * refactoring

---
+++ dezsi/Requirement2/Version2/ClientApp/ClientApp/ICommunication.cs
@@ -0,0 +1,19 @@
+﻿using CommonApp;
+using CommonApp.Model;
+using System;
+using System.Collections.Generic;
+
+
+namespace ClientApp
+{
+    interface ICommunication<T>
+    {
+        void SetUsername(string name);
+        string SendAndReceiveMessage<Obj>(Obj obj, Header type = Header.Unspecified);
+        void NewKey();
+        bool Login(string user, string pwd);
+        IList<Message> GetMessages();
+        Dictionary<string, string> GetUsers();
+        IList<Message> SendMessage(string user, string writeTo, string text, DateTime now);
+    }
+}
---
+++ dezsi/Requirement2/Version2/ClientApp/ClientApp/View/ChatWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using CommonApp;
+using CommonApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp.View
+{
+    /// <summary>
+    /// Interaction logic for ChatWindow.xaml
+    /// </summary>
+    public partial class ChatWindow :  Window
+    {
+        Services _client;
+
+        public ChatWindow()
+        {
+
+            InitializeComponent();
+
+        }
+      
+        public void SetUpUser() {
+
+            
+            _client.SetUp();
+
+            this.messagesFrom.ItemsSource = _client.Users.Keys.ToDictionary(e => e);
+            this.ChatBox.ItemsSource = _client.GetFilteredMessagesUser();
+        }
+
+        internal void AddService(Services client)
+        {
+            _client = client;
+        }
+
+        //a second setup based on user data
+        public void SetUserName(string username) {
+
+            _client.User = username;
+            
+         
+        }
+
+        private void messagesFrom_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+             
+            string getUser = ((KeyValuePair<string, string>)(this.messagesFrom.SelectedItem)).Value;
+            _client.WriteTo = getUser;
+          
+            this.ChatLabel.Content = " You can now text " + _client.WriteTo;
+            _client.SetMessages();
+            IList<Message> msg = _client.GetFilteredMessagesUsers();
+            this.ChatBox.ItemsSource = msg;
+
+        }
+
+
+        //Discussions between the two
+        
+
+        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            _client.SendMessage(this.messageText.Text, DateTime.Now);
+            _client.SetMessages();
+            this.ChatBox.ItemsSource = (IEnumerable<Message>)_client.GetFilteredMessagesUsers();
+
+
+        }
+
+    }
+}
