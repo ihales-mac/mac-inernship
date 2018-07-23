aec93d52996f63dbf7aaa1eac2f998188e4f526f
imoladezsiMacadamian <ikatalin-dezsi@macadamian.com>
2018-07-23 15:40:02 +0300

    filtered out some data

--- dezsi/python/Requirement2/serv.py
+++ dezsi/python/Requirement2/serv.py
@@ -37,6 +37,10 @@ class Serv(BaseHTTPRequestHandler):
             for i in range(0, len(data)):
                 data[i].pop("id")
                 data[i].pop("created")
+                data[i].pop("weather_state_abbr")
+                data[i].pop("applicable_date")
+                data[i].pop("wind_direction_compass")
+
 
             open(self.path[1:char], 'w').write("<html><head><title>{}</title></head><body>{}</body></html>"
                                                .format(param.capitalize()+" ("+str(self.get_date()[0])+"/"+str(self.get_date()[1])
@@ -46,13 +50,15 @@ class Serv(BaseHTTPRequestHandler):
 
         except Exception as e:
             print(e)
-            file_to_open = '<html style="background: repeating-linear-gradient(#c6e5f2, #c6e5f2 20px, #9198e5 20px, #9198e5 25px);">' \
+            file_to_open = '<html style="background: repeating-linear-gradient' \
+                           '(#c6e5f2, #c6e5f2 20px, #9198e5 20px, #9198e5 25px);">' \
                            '<head><title>404</title></head><body><div style= "border-radius: 360px; background: ' \
                            'radial-gradient(white, #24179b);position:relative; margin-left:600px; ' \
                            'margin-top:200px;height:150px; width:400px; border:4px solid black;">' \
                            '<h1 style="text-align: center;color:#000080">404</h1>' \
                            '<h2 style="color:#1725e8;text-align: center;">File Not Found</h2></div></body></html>'
             self.send_response(404)
+
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
