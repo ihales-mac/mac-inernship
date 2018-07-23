0a5f33b037d5a49fa0aef6fb46bdcca5822e812b
imoladezsiMacadamian <ikatalin-dezsi@macadamian.com>
2018-07-23 13:08:27 +0300

    weather

---
+++ dezsi/python/Requirement2/serv.py
@@ -0,0 +1,54 @@
+import datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.request
+from urllib.parse import urlparse
+import json
+
+
+class Serv(BaseHTTPRequestHandler):
+
+    @staticmethod
+    def get_date():
+        now = datetime.datetime.now()
+        return now.year, now.month, now.day
+
+    @staticmethod
+    def get_location(name):
+        with urllib.request.urlopen("https://www.metaweather.com/api/location/search/?query="+name) as url:
+            data = json.loads(url.read().decode())
+        return data[0]["woeid"]
+
+    def get_param(self, param):
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        return query_components[param]
+
+    def do_GET(self):
+
+        param = self.get_param("location")
+        try:
+            with urllib.request.urlopen("https://www.metaweather.com/api/location/{}/{}/{}/{}/".format(self.get_location(param), self.get_date()[0],
+                                                                                                       self.get_date()[1], self.get_date()[2]
+                                                                                                       )) as url:
+                data = json.loads(url.read().decode())
+            char = self.path.find('?')
+
+            #filter data
+            for i in range(0, len(data)):
+                data[i].pop("id")
+                data[i].pop("created")
+
+            open(self.path[1:char], 'w').write("{}".format(data))
+            file_to_open = open(self.path[1:char]).read()
+            self.send_response(200)
+        except Exception as e:
+            print(e)
+            file_to_open = 'File Not Found'
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('localhost', 8080), Serv)
+    httpd.serve_forever()
